3dc20e2f1aff5e4e7ac4bfbebc06f9ea06a6d51d
32-Mimhakkani <mim.cse32@gmail.com>
2022-03-15 10:45:32 +0600

    show the d3 tree eeents

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 /* eslint-disable react/jsx-pascal-case */
 
 import './App.css';
-import Orranizational_tree from './Components/Orranizational_tree';
+import D3_tree from './Components/D3/D3_tree';
+
+// import Orranizational_tree from './Components/Orranizational_tree';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Organizational Tree Example without json normal way is : </h1>
-       <Orranizational_tree />
+      {/* <h1>Organizational Tree Example without json normal way is : react-organizational-chart</h1> */}
+       {/* <Orranizational_tree /> */}
         
-        
+      <h1 style={{color:'red',marginTop:'15px'}}>D3 Tree Example with json api way is : react-d3-tree</h1>
+
+      <D3_tree />
+    
     </div>
   );
 }
---
+++ src/Components/D3/D3_tree.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Tree from "react-d3-tree";
+import orgChartJson from "./Data/org-chart.json";
+import { useCenteredTree } from "./helpers";
+// import "./styles.css";
+
+const containerStyles = {
+  width: "100vw",
+  height: "100vh"
+};
+
+// Here we're using `renderCustomNodeElement` to represent each node
+// as an SVG `rect` instead of the default `circle`.
+const renderRectSvgNode = ({ nodeDatum, toggleNode }) => (
+  <g>
+    <rect width="20" height="20" x="-10" onClick={toggleNode} />
+    <text fill="black" strokeWidth="1" x="20">
+      {nodeDatum.name}
+    </text>
+    {nodeDatum.attributes?.department && (
+      <text fill="black" x="20" dy="20" strokeWidth="1">
+        Department: {nodeDatum.attributes?.department}
+      </text>
+    )}
+  </g>
+);
+
+const D3_tree = () => {
+
+       const [dimensions, translate, containerRef] = useCenteredTree();
+  return (
+    <div style={containerStyles} ref={containerRef}>
+      <Tree
+        data={orgChartJson}
+        dimensions={dimensions}
+        translate={translate}
+        renderCustomNodeElement={renderRectSvgNode}
+        orientation="vertical"
+      />
+    </div>
+    );
+};
+
+export default D3_tree;
